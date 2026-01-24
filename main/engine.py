bfb8513aba2b25a9cf90eb142d24263c16c221c5
Ayush Devadiga <ayushdevadiga2005@gmail.com>
2026-01-24 14:41:03 +0530

    Template Created for Our Streamlit App

---
+++ main/engine.py
@@ -0,0 +1,22 @@
+import joblib
+from pathlib import Path
+
+def load_data(X):
+    pass
+
+def load_pipeline():
+    
+    MODEL_PATH = Path("./models/xgb_loan_pipeline.pkl")
+    pipeline = None
+    try:
+        pipeline = joblib.load(MODEL_PATH)
+        print('The Model / Pipeline was Loaded Successfully.')
+    except Exception as e:
+        print(f'Oops ! Unable To load Model / Pipeline \n Error Information : {e}')
+
+    return pipeline
+
+def main():
+    pass
+
+x = load_pipeline()
